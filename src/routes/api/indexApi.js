e2e69f0a1be12f9d194d39a7dcde8f8539002377
Nicolas Lopez <nicolasmlopez00@gmail.com>
2023-09-05 13:09:31 -0300

    sprint-16

--- src/routes/api/indexApi.js
+++ src/routes/api/indexApi.js
@@ -5,6 +5,22 @@ import auth_router from "./auth.js";
 import mocking_router from "./mockingProducts.js";
 import loggerRouter from "./loggerTest.js";
 import reset_pass_router from "./resetPassRoute.js";
+import { __dirname } from "../../utils.js";
+import swaggerUiExpress from "swagger-ui-express";  
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Documentacion del ecommerce',
+            description: 'E-commerce de automoviles'
+        }
+    },
+    apis:[`${__dirname}/docs/**/*.yaml`]
+}
+
+const specs = swaggerJSDoc(swaggerOptions)
 
 const index_api_router = Router()
 
@@ -14,5 +30,6 @@ index_api_router.use('/auth', auth_router)
 index_api_router.use('/mockingproducts', mocking_router)
 index_api_router.use('/loggerTest', loggerRouter)
 index_api_router.use('/resetPassword', reset_pass_router)
+index_api_router.use('/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
 
 export default index_api_router 
